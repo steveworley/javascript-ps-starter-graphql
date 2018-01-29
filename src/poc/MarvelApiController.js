89904b4de25c6b1dc4e9fe135de68650d95839cd
Steve Worley <steven.worley@acquia.com>
2018-01-29 22:48:14 +1000

    Cache plugins to a data controller.
    - Data controller instructs the application on how to fetch data from an
    endpoint. The data controller can specify which cache backend it wants
    to use for the requests.
    - Adds a simple interface for cache plugins.
    - Adds a simple interface for the data controllers.
    - Adds a marvel api data controller.

---
+++ src/poc/MarvelApiController.js
@@ -0,0 +1,72 @@
+import crypto from 'crypto';
+import fetch from 'node-fetch';
+import DataControllerBase from './DataControllerBase';
+import CacheLocal from './CacheLocal';
+
+// Import classes to assist with formatting the API response into a structure
+// that represents the GraphQL Schema for the type in the graphql server.
+import { Model as Comic } from '../types/comic';
+
+/**
+---- ENVIRONMENT VARIABLES ----
+dotenv stores configuration variables in a local .env file. This allows us to
+store API credentials locally and rely on environment variables in production
+to store important credentials.
+
+dotenv will not override environment defined variables with those in the file.
+
+@see https://github.com/motdotla/dotenv
+@see https://github.com/motdotla/dotenv/issues/133#issuecomment-321779690
+*/
+
+class MarvelApiController extends DataControllerBase {
+
+  cachePlugin = CacheLocal;
+
+  constructor() {
+    super();
+    const privateKey = process.env.API_PRIVATE_KEY;
+    const publicKey = process.env.API_PUBLIC_KEY;
+
+    const ts = Date.now();
+    const hash = crypto.createHash('md5').update(`${ts}${privateKey}${publicKey}`).digest('hex');
+
+    this.url = 'http://gateway.marvel.com/v1/public';
+    this.params = `ts=${ts}&apikey=${publicKey}&hash=${hash}`;
+  }
+
+  handleErrors(error) {
+    console.log(error);
+  }
+
+  characters() {
+    const url = `${this.url}/characters?${this.params}`;
+
+    return fetch(url)
+      .then(res => res.json())
+      .then()
+      .catch(this.handleErrors);
+  }
+
+  comics(id) {
+    const url = `${this.url}/characters/${id}/comics?${this.params}`;
+
+    if (this.getCache(`comics.${id}`)) {
+      return this.getCache(`comics.${id}`);
+    }
+
+    return fetch(url)
+      .then(res => res.json())
+      .then(json => {
+        const comics = json.data.results.map(i => new Comic(i));
+        this.setCache(`comics.${id}`, comics);
+        return comics;
+      })
+      .catch(() => this.getCache(`comics.${id}`));
+  }
+
+}
+
+// This is the singleton pattern for NodeJS - this will allow all types to use
+// the same instance of MarvelApi.
+export let api = new MarvelApiController();
