95fca5e38b5aa52d0ee530b52de8c5e37ddd846b
Steve Worley <steven.worley@acquia.com>
2018-02-12 16:59:20 +1000

    Schema.js documentation

--- src/schema.js
+++ src/schema.js
@@ -1,10 +1,16 @@
 import { bundle } from 'graphql-modules';
 import { makeExecutableSchema } from 'graphql-tools';
 
+/**
+ * --- Import types from the definitions ---
+ * Types are defined as submodules in the types directory (see types/README.md)
+ * and imported here to be exposed to the GraphQL server.
+ */
 import heroes from './types/hero';
 import villains from './types/villain';
 import comics from './types/comic';
 
+// Modules is passed to `bundle` which merges definitions to the root object.
 const modules = [heroes, villains, comics];
 
 export default makeExecutableSchema(bundle(modules));
